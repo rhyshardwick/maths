dc31fdaffe754225403095ce2ce08f2846ce3390
Rhys Hardwick <rhys@RhysLegion7>
2022-10-02 22:43:29 +0100

    Committer: Rhys Hardwick <rhys@RhysLegion7>
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    new file:   lucky_numbers.py

---
+++ numbers/lucky_numbers.py
@@ -0,0 +1,10 @@
+# Write ther first n Lucky numbers
+# 1) list all integers until limit
+# 2) remove all second numbers
+# 3) read second number (n) and remove every nth number
+# 4) repeat
+
+CalculateUntil = int(input("Calulate Lucky numbers up until: "))
+
+LuckyNumberes = [1]
+
